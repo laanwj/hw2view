6d182a96980fb05ac59581d49091909c4e14f651
laanwj <126646+laanwj@users.noreply.github.com>
2022-03-20 15:59:19 +0100

    Silence warning about missing "_transformations" module

--- transformations.py
+++ transformations.py
@@ -1869,40 +1869,6 @@ def is_same_transform(matrix0, matrix1):
     return numpy.allclose(matrix0, matrix1)
 
 
-def _import_module(name, package=None, warn=True, prefix='_py_', ignore='_'):
-    """Try import all public attributes from module into global namespace.
-
-    Existing attributes with name clashes are renamed with prefix.
-    Attributes starting with underscore are ignored by default.
-
-    Return True on successful import.
-
-    """
-    import warnings
-    from importlib import import_module
-    try:
-        if not package:
-            module = import_module(name)
-        else:
-            module = import_module('.' + name, package=package)
-    except ImportError:
-        if warn:
-            warnings.warn("failed to import module %s" % name)
-    else:
-        for attr in dir(module):
-            if ignore and attr.startswith(ignore):
-                continue
-            if prefix:
-                if attr in globals():
-                    globals()[prefix + attr] = globals()[attr]
-                elif warn:
-                    warnings.warn("no Python implementation of " + attr)
-            globals()[attr] = getattr(module, attr)
-        return True
-
-
-_import_module('_transformations')
-
 if __name__ == "__main__":
     import doctest
     import random  # used in doctests
